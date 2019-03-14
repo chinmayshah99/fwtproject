80d9ed7e78b05256f472d3d3f28587b163149373
Chinmay Shah <chinmayshah3899@gmail.com>
2019-03-14 23:59:20 +0530

    ported import to es6 guidline

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // import 
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyparser = require('body-parser');
-var cors = require('cors');
-var path = require('path');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
 
 
 var app = express();
