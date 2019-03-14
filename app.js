428263166289f728287781ad4ebb87ec67b98b76
Chinmay Shah <chinmayshah3899@gmail.com>
2019-03-14 23:56:30 +0530

    added packages

---
+++ app.js
@@ -0,0 +1,14 @@
+// import 
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+
+var app = express();
+
+
+// port
+const port = 3000;
+
