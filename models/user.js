7ed95727c2b799f04025defaabbb9e809b3ebb33
Chinmay Shah <chinmayshah3899@gmail.com>
2019-03-26 19:44:17 +0530

    modified user schema

--- models/user.js
+++ models/user.js
@@ -3,9 +3,17 @@
 var mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    first_name:{
+    email:{
         type: String,
         unique: true,
+       required: true
+    },
+    password:{
+        type: String,
+       required: true
+    },
+    first_name:{
+        type: String,
        required: true
     },
     last_name:{
